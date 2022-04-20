941746b3f1e8673eb4ad4b6ccb89110589aa875c
Stanley Nhampossa <Nhampossastanley@gmail.com>
2022-04-20 10:11:31 +0200

    first commit

---
+++ fichas_de_exercicios/fichanr4/Ex5_Pessoa.java
@@ -0,0 +1,9 @@
+package fichas_de_exercicios.fichanr4;
+
+public class Ex5_Pessoa {
+
+    private String nome;
+    private String localDeNascimento;
+    private int idade;
+
+}
---
+++ fichas_de_exercicios/fichanr4/Ex5_Produto.java
@@ -0,0 +1,4 @@
+package fichas_de_exercicios.fichanr4;
+
+public class Ex5_Produto {
+}
